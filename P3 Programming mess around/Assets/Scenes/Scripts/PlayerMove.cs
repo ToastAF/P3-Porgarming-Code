5a387232d6b4d68915593d05f430d79e801d6be7
Jakob <jped22@student.aau.dk>
2023-09-13 23:31:38 +0200

    Added navmesh and such

---
+++ P3 Programming mess around/Assets/Scenes/Scripts/PlayerMove.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody rb;
+    public float moveSpeed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(-moveSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(0, 0, -moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(0, 0, moveSpeed * Time.deltaTime);
+        }
+    }
+}
