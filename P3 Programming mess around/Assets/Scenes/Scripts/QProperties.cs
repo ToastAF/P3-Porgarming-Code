d578b68d144508bffe73a72e9f2252006cc67327
Jakob K. Pedersen <37065647+BaconToast420@users.noreply.github.com>
2023-09-20 12:25:17 +0200

    Mellemting

--- P3 Programming mess around/Assets/Scenes/Scripts/QProperties.cs
+++ P3 Programming mess around/Assets/Scenes/Scripts/QProperties.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class QProperties : MonoBehaviour
 {
     Rigidbody rb;
+    GameObject player;
     public LayerMask clickableThings;
+    public int projectileSpeed;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
         StartCoroutine(DestroySelf());
         
         Ray pointRay = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -17,7 +20,8 @@ public class QProperties : MonoBehaviour
         if (Physics.Raycast(pointRay, out hitInfo, 100, clickableThings))
         {
             print("I'm doing something!");
-            rb.velocity = hitInfo.point; //Lav vektor regning mellem player og mus :)
+            Vector3 mousePlayerVec = new Vector3(hitInfo.point.x, hitInfo.point.y + player.transform.position.y, hitInfo.point.z) - player.transform.position;
+            rb.velocity = mousePlayerVec.normalized * projectileSpeed; //Lav vektor regning mellem player og mus :)
         }  
     }
 
