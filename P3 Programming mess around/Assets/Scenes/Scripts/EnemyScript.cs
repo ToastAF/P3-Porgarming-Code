af546776cbf7b6c59a58d71da3ee30b19ad1dc4a
Jakob K. Pedersen <37065647+BaconToast420@users.noreply.github.com>
2023-09-21 14:01:11 +0200

    Enemy can die and take damage based on projectile
    Also projectile dies

--- P3 Programming mess around/Assets/Scenes/Scripts/EnemyScript.cs
+++ P3 Programming mess around/Assets/Scenes/Scripts/EnemyScript.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    float health;
+    float health, armor, magicResist;
     // Start is called before the first frame update
     void Start()
     {
         health = 20;
+        armor = 5;
+        magicResist = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Projectile"))
+        {
+            QProperties newScript = other.gameObject.GetComponent<QProperties>();
+            Destroy(other.gameObject);
+            health -= newScript.physDmg + newScript.magDmg;
+            print("DAMAGE! Current health: " + health);
+        }
+    }
+
+    void calculateDamage()
+    {
+
     }
 }
--- P3 Programming mess around/Assets/Scenes/Scripts/QProperties.cs
+++ P3 Programming mess around/Assets/Scenes/Scripts/QProperties.cs
@@ -30,7 +30,7 @@ public class QProperties : MonoBehaviour
         
         if (Physics.Raycast(pointRay, out hitInfo, 100, clickableThings))
         {
-            print("I'm doing something!");
+            //print("I'm doing something!");
             mousePlayerVec = new Vector3(hitInfo.point.x, hitInfo.point.y + player.transform.position.y, hitInfo.point.z) - player.transform.position;
             transform.LookAt(hitInfo.point);
             rb.velocity = mousePlayerVec.normalized * projectileSpeed; //Lav vektor regning mellem player og mus :)
